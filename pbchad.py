dff45e75bde7118faa3c0c95528f780b8f1fb37e
Alex Riasanovsky <alexander.riasanovsky@gmail.com>
2019-07-27 13:08:54 -0400

    cleaning up comments, print lines, deprecated functions

--- pbchad.py
+++ pbchad.py
@@ -8,7 +8,7 @@ from numpy import matrix
 
 import sys
 
-from pysat.solvers import Glucose4
+from pysat.solvers import Glucose3, Glucose4, Lingeling, MapleChrono, MapleCM, Maplesat, Minicard, Minisat22, MinisatGH
 from pysat.pb import *
 
 import datetime
@@ -16,14 +16,7 @@ import time
 
 #debug variables; delete once code is good!
 VERBOSE = True
-VERBOSE_SIMP = False
-VERBOSE_HADS = False
-VERBOSE_MULTS = False
-VERBOSE_LIST = False
-VERBOSE_OR = False
-VERBOSE_SOLS = True
-TIDYING = False
-OLD_CARDINALITY = True
+DEBUGGING = False
 
 TO_HADS = "./hads/"
 TO_CNFS = "./cnfs/"
@@ -39,10 +32,6 @@ def diagonal_matrix(x):
         currow[i] = x[i]
         d.append(currow)
     
-    #print "diag of", x, "is:"
-    #for r in d:
-        #print r, 'of length', len(r)
-    #print
     return matrix(d)
 
 
@@ -57,100 +46,6 @@ def swap_cols(M,i,j):
         N[k,i]=N[k,j]
         N[k,j]=x[k]
     return N
-
-
-
-#takes in a list of tuples corresponding to a CNF formula 
-#returns a shorter list for an equivalent formula
-#only simplifies if it finds clauses c1 c2 where: 
-    #c1 = a|b and c2 = a|~b|c
-    #generally, c1&c2 = (a|b)&(a|~b|c) <==> (a|b)&(a|c)
-    #if b or c is the empty formula, then c1&c2 <==> a
-
-#assumes no tuple in old_tups has both i and -i
-def simplify_clause_list(old_tups):
-    
-    #num_simps = 0
-    #num_removals = 0
-    #curr_thresh = 10
-    #num_reboots = 0
-    
-    irredundants = dict()
-    
-    for i in range(len(old_tups)):      #[0. .len(old_tups)-1]:
-        irredundants[i] = set(old_tups[i])
-    
-    progressing = True
-    while progressing:
-        progressing = False
-        for i in range(len(old_tups)):#[0. .len(old_tups)-1]:
-            if i in irredundants:
-                for j in range(i+1, len(old_tups)):#[i+1. .len(old_tups)-1]:
-                    if j in irredundants and i in irredundants:
-                        #if num_simps >= curr_thresh:
-                            #print 'num simps, removals:', num_simps, num_removals, "num reboots:", num_reboots
-                            #print 'i,j=', i,j
-                            #curr_thresh = num_simps/100
-                            #curr_thresh = (curr_thresh+1)*100
-                        sj = irredundants[j]
-                        si = irredundants[i]
-                        inters = si.intersection(sj)
-                        diffi = si.difference(sj)
-                        diffj = sj.difference(si)
-                        
-                        
-                        #first check if one is a subset of the other
-                        #if so, delete the bigger one
-                        if len(diffi) == 0:
-                            dead_ind = irredundants.pop(j)
-                            progressing = True
-                            #num_simps += 1
-                            #num_removals += 1
-                        else:
-                            if len(diffj) == 0:
-                                dead_ind = irredundants.pop(i)
-                                progressing = True
-                                #num_simps += 1
-                                #num_removals += 1
-                            else:
-                                
-                                
-                                #checking to see if rows i and j 
-                                #are of the form a|b, and a|~b|c
-                                #where b is a single variable in diffi or diffj
-                                #and a is the "intersection" of the clauses
-                                #note that (a|b)&(a|~b|c) <==> (a|b)&(a|c)
-                                #and if c is vacuous, then instead we have 
-                                #(a|b)&(a|~b) <==> a
-                                if len(diffi) == 1:
-                                    b = list(diffi)[0]
-                                    if -b in sj:
-                                        if len(diffj) == 1:
-                                            dead_index = irredundants.pop(j)
-                                            si.remove(b)
-                                            irredundants[i] = si
-                                            progressing = True
-                                            #num_simps += 1
-                                            #num_removals += 1
-                                        else:
-                                            sj.remove(-b)
-                                            irredundants[j] = sj
-                                            progressing = True
-                                            #num_simps += 1
-                                else:
-                                    if len(diffj) == 1:
-                                        b = list(diffj)[0]
-                                        if -b in si:
-                                            si.remove(-b)
-                                            irredundants[i] = si
-                                            progressing = True
-                                            #num_simps += 1
-        #num_reboots += 1
-    new_tups = []
-    for i in irredundants:
-        new_tups.append( irredundants[i] )
-    return new_tups
-
 
 
 #checks to see if c1 = A|B and c2 = A|~B|C somehow
@@ -247,7 +142,7 @@ def relabel(good_clauses, a, b):
         if a in c1:
             if -a in c1 or -b in c1:
                 #clause is always satisfied
-                to_pop.append(i)#dud = good_clauses.pop(i)
+                to_pop.append(i)
                 if DEBUGGING:
                     print '        c1=', c1, 'so toss it!'
             else:
@@ -264,7 +159,7 @@ def relabel(good_clauses, a, b):
                     if DEBUGGING:
                         print '        c1=', c1, 'so toss it!'
                     #clause is always satisfied
-                    to_pop.append(i)#dud = good_clauses.pop(i)
+                    to_pop.append(i)
                 else:
                     if -b in c1:
                         if DEBUGGING:
@@ -279,7 +174,7 @@ def relabel(good_clauses, a, b):
                         if DEBUGGING:
                             print '        c1=', c1, 'so toss it!'
                         #clause is always satisfied
-                        to_pop.append(i)#dud = good_clauses.pop(i)
+                        to_pop.append(i)
                     else:
                         if DEBUGGING:
                             print '        c1=', c1, 'so swap', b, 'with', a
@@ -404,11 +299,17 @@ def remove_singletons(good_clauses, dummy_threshold):
 
 #if there exist two variables x_i, x_j (i<j) where: 
     #clauses x_i|~x_j and ~x_i|x_j exist, then replace x_j with x_i 
+    #if j >= dummy_threshold, remove x_j entirely
 
-#if dummy_threshold != 0 and dummy_threshold = 
-    #lower one in all expressions and remove the clauses
+#if there exists a singleton clause x_i (or ~x_i), then:
+    #for all clauses c containing x_i, remove the entire clause
+    #for all clauses c containing ~x_i, remove ~x_i from the clause
+
+#once no such simplifications exist, shift all dummy variables 
+#down to the lowest possible names for the dummy variables
+
 #assumes no tuple in old_tups has both i and -i
-def simplify_clause_list2(old_clauses, dummy_threshold):
+def simplify_clause_list(old_clauses, dummy_threshold):
     
     #keep a dictionary of all good clauses, by their original index
     good_clauses = dict()
@@ -433,7 +334,7 @@ def simplify_clause_list2(old_clauses, dummy_threshold):
         
         
         removed_singles = remove_singletons(good_clauses, dummy_threshold)
-        if removed_singles and DEBUGGING:
+        if DEBUGGING and removed_singles:
             print 'removed singles!'
         
             print 'now only', len(good_clauses), 'clauses!'
@@ -441,7 +342,7 @@ def simplify_clause_list2(old_clauses, dummy_threshold):
             show_zeroed_clauses( good_clauses, [-i for i in range(1,dummy_threshold)] )
         
         subbed = simplify_by_substitutions(good_clauses, dummy_threshold)
-        if subbed and DEBUGGING:
+        if DEBUGGING and subbed:
             print 'subbed!'
         
             print 'now only', len(good_clauses), 'clauses!'
@@ -449,7 +350,7 @@ def simplify_clause_list2(old_clauses, dummy_threshold):
             show_zeroed_clauses( good_clauses, [-i for i in range(1,dummy_threshold)] )
         
         abc_simplified = abc_simplifier(good_clauses)
-        if abc_simplified and DEBUGGING:
+        if DEBUGGING and abc_simplified:
             print 'abc simplified!'
 
             print 'now only', len(good_clauses), 'clauses!'
@@ -468,7 +369,7 @@ def simplify_clause_list2(old_clauses, dummy_threshold):
         #return 
     
     shifted = dummy_shift(good_clauses, dummy_threshold)
-    if shifted and DEBUGGING:
+    if DEBUGGING and shifted:
         print 'shifted!'
     
         print 'now only', len(good_clauses), 'clauses!'
@@ -485,7 +386,6 @@ def simplify_clause_list2(old_clauses, dummy_threshold):
 #helper method for debugging; helps show if a clause is 
 #satisfied by all-false truth assignments
 def show_zeroed_clauses(good_clauses, assumed):
-    return 
     print 'assumed', assumed
     for i in good_clauses:
         reduced = [j for j in good_clauses[i] if not -j in assumed]
@@ -603,7 +503,14 @@ def hads_to_graphs(infile_names, outfile_names, all_columns = True, transpose = 
             nclauses = 0
             nvars = K*(K-1)/2
             
-            g = Glucose4()
+            g = Glucose3()
+            #g = Glucose4()
+            #g = Lingeling()
+            #g = MapleChrono()
+            #g = MapleCM()
+            #g = Maplesat()
+            #g = Minisat22()
+            #g = MinisatGH()
             
             matchings = dict()
             
@@ -617,8 +524,8 @@ def hads_to_graphs(infile_names, outfile_names, all_columns = True, transpose = 
                 my_row = tuple(my_row)
                 matchings[my_row] = ((ectr,0,b))
             
-            for a in range(1,K):      #[0. .K-1]:
-                for b in range(a+1,K):   #[a+1. .K-1]:
+            for a in range(1,K):
+                for b in range(a+1,K):
                     if DEBUGGING:
                         print 'trying', (a,b)
                     ectr += 1
@@ -630,7 +537,6 @@ def hads_to_graphs(infile_names, outfile_names, all_columns = True, transpose = 
                             dot += hab[k]*hh[j,k]
                         my_row.append(dot)
                     
-                    #my_row.append(-K)
                     my_row = tuple(my_row)
                     
                     if my_row in matchings:
@@ -662,7 +568,7 @@ def hads_to_graphs(infile_names, outfile_names, all_columns = True, transpose = 
                             old_ectr, num_new_vars, old_coes_to_inds, old_clauses = mults_to_clauses[mults]
                             
                             #must relabel the old indices to match the new ones
-                            #for each coefficient c (including -K)
+                            #for each coefficient c 
                             relab = dict()
                             
                             #print 'trying to relabel with', old_coes_to_inds
@@ -675,7 +581,6 @@ def hads_to_graphs(infile_names, outfile_names, all_columns = True, transpose = 
                                     relab[-1-old_index] = -1-coes_to_inds[coe][coe_ctr]
                                     coe_ctr += 1
                             
-                            #print 'is old_ectr already relabeled???', old_ectr in relab
                             
                             relab[old_ectr] = ectr
                             relab[-old_ectr] = -ectr
@@ -683,10 +588,6 @@ def hads_to_graphs(infile_names, outfile_names, all_columns = True, transpose = 
                             for i in range(1,1+num_new_vars):
                                 relab[old_ectr+i] = i+nvars
                                 relab[-old_ectr-i] = -i-nvars
-                            
-                            #print 'the relabeling is:'
-                            #for k in relab:
-                            #    print '    ', k, relab[k]
                             
                             for curr_clause in old_clauses:
                                 g.add_clause([relab[k] for k in curr_clause])
@@ -697,7 +598,7 @@ def hads_to_graphs(infile_names, outfile_names, all_columns = True, transpose = 
                         else:
                             print 'dealing with', mults,'for the first time...'
                             cnf = PBEnc.equals( lits = range(1,K) + [ectr], weights = list(my_row)+[-K], bound = 0, encoding = 4 )
-                            curr_clauses, maxvar = simplify_clause_list2(cnf.clauses, ectr + 1)
+                            curr_clauses, maxvar = simplify_clause_list(cnf.clauses, ectr + 1)
                             
                             num_new_vars = maxvar - ectr
                             
@@ -734,40 +635,22 @@ def hads_to_graphs(infile_names, outfile_names, all_columns = True, transpose = 
                             if DEBUGGING:
                                 print 'now there are', (nvars, nclauses), 'vars,clauses'
                             
-                            #if ectr == 33:
-                            #    return 
-                            if b==31:
-                                print 'at', (a,b),',', (nvars, nclauses), 'vars and clauses'
-                                print(datetime.datetime.now().time())
-                            #    return 
-                            
-                            #print 'found', len(cnf.clauses)
-                            #print 
-                
             sol_ctr = 0
             
-            if True or DEBUGGING:
+            if VERBOSE:
                 print 'there are', nvars, 'variables and', nclauses, 'clauses'
                 print 'trying to find all solutions!'
-                #print(datetime.datetime.now().time())
-                #start_time = datetime.datetime.now()
-            
+                
             while g.solve():
                 new_sol = g.get_model()
-                #print "sol #", sol_ctr, ':'
-                #print 
                 sol_ctr += 1
-                #if sol_ctr % 1000 == 0:
-                #    print sol_ctr,'solutions...'
                 nx.write_graph6(nx.Graph([emap[k] for k in new_sol[:K*(K-1)/2] if k > 0]), outmaster, nodes = range(K))
                 g.add_clause( [-new_sol[j] for j in range(K-1)] )
-                #outmaster.write( str(new_sol[:496]) + ",\n" )
             
             hctr += 1
             
-            if True or debugging:
+            if VERBOSE:
                 print sol_ctr, 'solutions found!'
-                #print(datetime.datetime.now().time())
                 end_time = datetime.datetime.now()
                 elapsed_time = end_time - start_time
                 print 'total time elapsed:', elapsed_time.seconds,":",elapsed_time.microseconds, 'matrices solved:', hctr
@@ -784,7 +667,6 @@ def hads_to_graphs(infile_names, outfile_names, all_columns = True, transpose = 
     
     outmaster.close()
 
-DEBUGGING = False
 
 infile_name = sys.argv[1]
 outfile_name = sys.argv[2]
